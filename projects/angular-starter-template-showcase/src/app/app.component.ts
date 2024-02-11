af862fe40f5332a72756bf732f61fa6fd703f3f2
SebastianHatnean <sebastian.hatnean@gmail.com>
2024-02-11 15:40:47 +0200

    consume library and showcase

--- projects/angular-starter-template-showcase/src/app/app.component.ts
+++ projects/angular-starter-template-showcase/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AngularStarterTemplateComponent } from '../../../angular-starter-template/src/lib/angular-starter-template.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, AngularStarterTemplateComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-starter-template-showcase';
--- projects/angular-starter-template/src/public-api.ts
+++ projects/angular-starter-template/src/public-api.ts
@@ -2,5 +2,4 @@
  * Public API Surface of angular-starter-template
  */
 
-export * from './lib/angular-starter-template.service';
 export * from './lib/angular-starter-template.component';
